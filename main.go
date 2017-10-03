1a099d3241770e3d606c7fc5d2d4484cd90570ae
Wills Ward <willseward@sbcglobal.net>
2017-10-03 16:33:43 -0500

    remove mysql, sqlite, crate, and bash drivers

--- main.go
+++ main.go
@@ -4,13 +4,9 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/db-journey/bash-driver"
 	_ "github.com/db-journey/cassandra-driver"
-	_ "github.com/db-journey/crate-driver"
 	journey "github.com/db-journey/journey/commands"
-	_ "github.com/db-journey/mysql-driver"
 	_ "github.com/db-journey/postgresql-driver"
-	_ "github.com/db-journey/sqlite3-driver"
 	"github.com/urfave/cli"
 )
 
